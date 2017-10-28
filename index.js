699242904ffbcfff22047e4704a40c4161f51277
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-10-28 18:18:18 +0700

    sample using socket.io

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+const console = require('tracer').colorConsole();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/chat.html');
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+  	var address = socket.handshake.address;
+  	console.log(socket.id, address);
+  	console.log('msg:', msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log('listening on *:' + port);
+});
